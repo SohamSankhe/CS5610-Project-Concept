e122a00bcd10a40fe39c127beb59b7b802a76a29
sohamsankhe <soham.sankhe@gmail.com>
2019-10-17 19:28:05 -0400

    Fixed errors in dictionary.ex and added httpoison as dep

--- lib/scrabble/dictionary.ex
+++ lib/scrabble/dictionary.ex
@@ -4,36 +4,16 @@ defmodule DictionaryApi do
     url = "https://dictionaryapi.com/api/v3/references/collegiate/json/" <> word <> "?key=eefa30f1-c7f2-44e4-a14e-4d48da208517"
     response = HTTPoison.get!(url)
     req = Poison.decode!(response.body)
+
     if req == [] do
-      return false 
+      false
+    end
+
     [head | tail] = req
     if is_map(head) do
-      return true
+      true
     end
-    return false
+
+    false
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
